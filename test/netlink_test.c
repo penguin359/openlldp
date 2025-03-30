2e9f25c43846c106b561c377e0afc69c2ea13a7b
Loren M. Lang <loren.lang@intel.com>
2025-03-30 02:10:58 -0700

    Switch netlink tests to use check test framework

--- test/netlink_test.c
+++ test/netlink_test.c
@@ -1,4 +1,3 @@
-#include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,6 +9,8 @@
 
 #include <netlink/msg.h>
 
+#include <check.h>
+
 #include <lldp_util.h>
 
 extern int get_mac2(int s, const char *ifname, u8 mac[], bool perm_mac);
@@ -18,7 +19,7 @@ extern int get_mac2(int s, const char *ifname, u8 mac[], bool perm_mac);
 int __wrap_ioctl(int fd, unsigned long op, void *arg)
 {
 	fprintf(stderr, "ioctl(%d, %lu, %p)\n", fd, op, arg);
-	assert(op == SIOCGIFINDEX);
+	ck_assert_int_eq(SIOCGIFINDEX, op);
 
 	return 0;
 }
@@ -37,7 +38,7 @@ struct nl_msg *build_getlink_msg(const uint8_t mac[6], const char *kind, const u
 	if(nlmsg_append(msg, &ifi, sizeof(ifi), NLMSG_ALIGNTO) != 0)
 		goto out_err;
 	NLA_PUT_STRING(msg, IFLA_IFNAME, "eth0");
-	//assert(sizeof(mac) == 6);
+	//ck_assert_int_eq(6, sizeof(mac));
 	//NLA_PUT(msg, IFLA_ADDRESS, sizeof(mac), mac);
 	if((addr = nl_addr_build(AF_LLC, mac, 6)) == NULL)
 		goto out_err;
@@ -76,7 +77,7 @@ out_err:
 static const uint8_t dummy_mac[] = { 0x11, 0x22, 0x33, 0x44, 0x55, 0x66 };
 static const uint8_t dummy2_mac[] = { 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff };
 
-void test_basic_get_mac(void)
+START_TEST(test_basic_get_mac)
 {
 	uint8_t result_mac[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 	int socket_pair[2];
@@ -85,7 +86,7 @@ void test_basic_get_mac(void)
 	struct nl_msg *msg = NULL;
 
 	msg = build_getlink_msg(dummy_mac, NULL, NULL);
-	assert(msg != NULL);
+	ck_assert_ptr_nonnull(msg);
 
 	if (socketpair(AF_UNIX, SOCK_SEQPACKET, 0, socket_pair) < 0) {
 		perror("socketpair failed");
@@ -101,13 +102,14 @@ void test_basic_get_mac(void)
 	ret = get_mac2(socket_pair[0], "eth0", result_mac, false);
 	close(socket_pair[1]);
 	//printf("After: %02x:%02x:%02x:%02x:%02x:%02x\n", result_mac[0], result_mac[1], result_mac[2], result_mac[3], result_mac[4], result_mac[5]);
-	assert(ret == 0);
-	assert(memcmp(result_mac, dummy_mac, sizeof(result_mac)) == 0);
+	ck_assert_int_eq(0, ret);
+	ck_assert_mem_eq(dummy_mac, result_mac, sizeof(result_mac));
 
 	nlmsg_free(msg);
 }
+END_TEST
 
-void test_ext_get_mac(void)
+START_TEST(test_ext_get_mac)
 {
 	uint8_t result_mac[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 	int socket_pair[2];
@@ -116,7 +118,7 @@ void test_ext_get_mac(void)
 	struct nl_msg *msg = NULL;
 
 	msg = build_getlink_msg(dummy_mac, NULL, NULL);
-	assert(msg != NULL);
+	ck_assert_ptr_nonnull(msg);
 
 	if (socketpair(AF_UNIX, SOCK_SEQPACKET, 0, socket_pair) < 0) {
 		perror("socketpair failed");
@@ -130,13 +132,14 @@ void test_ext_get_mac(void)
 
 	ret = get_mac2(socket_pair[0], "eth0", result_mac, true);
 	close(socket_pair[1]);
-	assert(ret == 0);
-	assert(memcmp(result_mac, dummy_mac, sizeof(result_mac)) == 0);
+	ck_assert_int_eq(0, ret);
+	ck_assert_mem_eq(dummy_mac, result_mac, sizeof(result_mac));
 
 	nlmsg_free(msg);
 }
+END_TEST
 
-void test_bond_get_mac(void)
+START_TEST(test_bond_get_mac)
 {
 	uint8_t result_mac[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 	int socket_pair[2];
@@ -145,7 +148,7 @@ void test_bond_get_mac(void)
 	struct nl_msg *msg = NULL;
 
 	msg = build_getlink_msg(dummy_mac, "bond", dummy2_mac);
-	assert(msg != NULL);
+	ck_assert_ptr_nonnull(msg);
 
 	if (socketpair(AF_UNIX, SOCK_SEQPACKET, 0, socket_pair) < 0) {
 		perror("socketpair failed");
@@ -159,21 +162,41 @@ void test_bond_get_mac(void)
 
 	ret = get_mac2(socket_pair[0], "eth0", result_mac, true);
 	close(socket_pair[1]);
-	assert(ret == 0);
-	assert(memcmp(result_mac, dummy2_mac, sizeof(result_mac)) == 0);
+	ck_assert_int_eq(0, ret);
+	ck_assert_mem_eq(dummy2_mac, result_mac, sizeof(result_mac));
 
 	nlmsg_free(msg);
+}
+END_TEST
+
+Suite *netlink_suite(void)
+{
+	Suite *s;
+	TCase *tc_get_mac;
+
+	s = suite_create("Netlink");
+
+	tc_get_mac = tcase_create("Get MAC");
+
+	tcase_add_test(tc_get_mac, test_basic_get_mac);
+	tcase_add_test(tc_get_mac, test_ext_get_mac);
+	tcase_add_test(tc_get_mac, test_bond_get_mac);
+	suite_add_tcase(s, tc_get_mac);
+
+	return s;
 }
 
 int main()
 {
-	//pthread_t sender_thread_id, receiver_thread_id;
+	int number_failed;
+	Suite *s;
+	SRunner *sr;
 
-	test_basic_get_mac();
-	test_ext_get_mac();
-	test_bond_get_mac();
+	s = netlink_suite();
+	sr = srunner_create(s);
 
-	printf("Test passed!\n");
-
-	return 0;
+	srunner_run_all(sr, CK_NORMAL);
+	number_failed = srunner_ntests_failed(sr);
+	srunner_free(sr);
+	return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
