a7a2796104e0fb0624da8a0bd7e3886101c957d5
Loren M. Lang <loren.lang@intel.com>
2025-03-30 23:05:26 -0700

    Use a test fixture to set up much of the common code for tests

--- test/netlink_test.ts
+++ test/netlink_test.ts
@@ -22,13 +22,12 @@ int __wrap_ioctl(int fd, unsigned long op, void *arg)
 	return 0;
 }
 
-struct nl_msg *build_getlink_msg(const uint8_t mac[6], const char *kind, const uint8_t slave_mac[6])
+struct nl_msg *build_getlink_msg(struct nl_addr *addr, const char *kind, struct nl_addr *slave_addr)
 {
 	struct nl_msg *msg = NULL;
 	struct ifinfomsg ifi;
 	struct nlattr *opts;
 	struct nlattr *opts2;
-	struct nl_addr *addr = NULL;
 
 	memset(&ifi, 0U, sizeof(ifi));
 	if((msg = nlmsg_alloc_simple(RTM_GETLINK, 0)) == NULL)
@@ -36,26 +35,16 @@ struct nl_msg *build_getlink_msg(const uint8_t mac[6], const char *kind, const u
 	if(nlmsg_append(msg, &ifi, sizeof(ifi), NLMSG_ALIGNTO) != 0)
 		goto out_err;
 	NLA_PUT_STRING(msg, IFLA_IFNAME, "eth0");
-	//ck_assert_int_eq(6, sizeof(mac));
-	//NLA_PUT(msg, IFLA_ADDRESS, sizeof(mac), mac);
-	if((addr = nl_addr_build(AF_LLC, mac, 6)) == NULL)
-		goto out_err;
 	NLA_PUT_ADDR(msg, IFLA_ADDRESS, addr);
-	nl_addr_put(addr);
-	addr = NULL;
 
-	if(kind || slave_mac) {
+	if(kind || slave_addr) {
 		if((opts = nla_nest_start(msg, IFLA_LINKINFO)) == NULL)
 			goto out_err;
 
 		if((opts2 = nla_nest_start(msg, IFLA_INFO_SLAVE_DATA)) == NULL)
 			goto out_err;
 
-		if((addr = nl_addr_build(AF_LLC, slave_mac, 6)) == NULL)
-			goto out_err;
-		NLA_PUT_ADDR(msg, IFLA_BOND_SLAVE_PERM_HWADDR, addr);
-		nl_addr_put(addr);
-		addr = NULL;
+		NLA_PUT_ADDR(msg, IFLA_BOND_SLAVE_PERM_HWADDR, slave_addr);
 		nla_nest_end(msg, opts2);
 		if(kind)
 			NLA_PUT_STRING(msg, IFLA_INFO_SLAVE_KIND, kind);
@@ -66,7 +55,6 @@ struct nl_msg *build_getlink_msg(const uint8_t mac[6], const char *kind, const u
 
 nla_put_failure:
 out_err:
-	nl_addr_put(addr);
 	nlmsg_free(msg);
 
 	return NULL;
@@ -75,24 +63,56 @@ out_err:
 static const uint8_t dummy_mac[] = { 0x11, 0x22, 0x33, 0x44, 0x55, 0x66 };
 static const uint8_t dummy2_mac[] = { 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff };
 
+static struct nl_addr *dummy_addr = NULL;
+static struct nl_addr *dummy2_addr = NULL;
+
+static int socket_pair[2];
+
+#define ck_assert_addr_eq(a, b)		do { \
+	char buf1[256]; \
+	char buf2[256]; \
+	ck_assert_msg(nl_addr_cmp((a), (b)) == 0, \
+		      "Expected addr %s != actual addr %s", \
+		      nl_addr2str((a), buf1, sizeof(buf1)), \
+		      nl_addr2str((b), buf2, sizeof(buf2))); \
+    } while(0)
+
+#define ck_assert_mac_is_addr(mac, addr)  do { \
+	struct nl_addr *mac_addr; \
+	ck_assert_ptr_nonnull(mac_addr = nl_addr_build(AF_LLC, (mac), sizeof(mac))); \
+	ck_assert_addr_eq((addr), mac_addr); \
+	nl_addr_put(mac_addr); \
+    } while(0)
+
+
+void netlink_setup(void)
+{
+	ck_assert_int_eq(0, socketpair(AF_UNIX, SOCK_SEQPACKET, 0, socket_pair));
+
+	ck_assert_ptr_nonnull(dummy_addr = nl_addr_build(AF_LLC, dummy_mac, sizeof(dummy_mac)));
+	ck_assert_ptr_nonnull(dummy2_addr = nl_addr_build(AF_LLC, dummy2_mac, sizeof(dummy2_mac)));
+}
+
+void netlink_teardown(void)
+{
+	// socket_pair[0] is expected to be closed by get_mac()
+	close(socket_pair[1]);
+
+	nl_addr_put(dummy_addr);
+	nl_addr_put(dummy2_addr);
+}
+
+
 #suite Netlink
 
 #tcase Get MAC
 
 #test test_basic_get_mac
 	uint8_t result_mac[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-	int socket_pair[2];
 	int ret;
 
-	struct nl_msg *msg = NULL;
-
-	msg = build_getlink_msg(dummy_mac, NULL, NULL);
+	struct nl_msg *msg = build_getlink_msg(dummy_addr, NULL, NULL);
 	ck_assert_ptr_nonnull(msg);
-
-	if (socketpair(AF_UNIX, SOCK_SEQPACKET, 0, socket_pair) < 0) {
-		perror("socketpair failed");
-		exit(EXIT_FAILURE);
-	}
 
 	if(send(socket_pair[1], nlmsg_hdr(msg), nlmsg_hdr(msg)->nlmsg_len, 0) < 0) {
 		perror("send() failed");
@@ -101,27 +121,18 @@ static const uint8_t dummy2_mac[] = { 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff };
 
 	//printf("Before: %02x:%02x:%02x:%02x:%02x:%02x\n", result_mac[0], result_mac[1], result_mac[2], result_mac[3], result_mac[4], result_mac[5]);
 	ret = get_mac2(socket_pair[0], "eth0", result_mac, false);
-	close(socket_pair[1]);
 	//printf("After: %02x:%02x:%02x:%02x:%02x:%02x\n", result_mac[0], result_mac[1], result_mac[2], result_mac[3], result_mac[4], result_mac[5]);
 	ck_assert_int_eq(0, ret);
-	ck_assert_mem_eq(dummy_mac, result_mac, sizeof(result_mac));
+	ck_assert_mac_is_addr(result_mac, dummy_addr);
 
 	nlmsg_free(msg);
 
 #test test_ext_get_mac
 	uint8_t result_mac[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-	int socket_pair[2];
 	int ret;
 
-	struct nl_msg *msg = NULL;
-
-	msg = build_getlink_msg(dummy_mac, NULL, NULL);
+	struct nl_msg *msg = build_getlink_msg(dummy_addr, NULL, NULL);
 	ck_assert_ptr_nonnull(msg);
-
-	if (socketpair(AF_UNIX, SOCK_SEQPACKET, 0, socket_pair) < 0) {
-		perror("socketpair failed");
-		exit(EXIT_FAILURE);
-	}
 
 	if(send(socket_pair[1], nlmsg_hdr(msg), nlmsg_hdr(msg)->nlmsg_len, 0) < 0) {
 		perror("send() failed");
@@ -129,26 +140,17 @@ static const uint8_t dummy2_mac[] = { 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff };
 	}
 
 	ret = get_mac2(socket_pair[0], "eth0", result_mac, true);
-	close(socket_pair[1]);
 	ck_assert_int_eq(0, ret);
-	ck_assert_mem_eq(dummy_mac, result_mac, sizeof(result_mac));
+	ck_assert_mac_is_addr(result_mac, dummy_addr);
 
 	nlmsg_free(msg);
 
 #test test_bond_get_mac
 	uint8_t result_mac[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-	int socket_pair[2];
 	int ret;
 
-	struct nl_msg *msg = NULL;
-
-	msg = build_getlink_msg(dummy_mac, "bond", dummy2_mac);
+	struct nl_msg *msg = build_getlink_msg(dummy_addr, "bond", dummy2_addr);
 	ck_assert_ptr_nonnull(msg);
-
-	if (socketpair(AF_UNIX, SOCK_SEQPACKET, 0, socket_pair) < 0) {
-		perror("socketpair failed");
-		exit(EXIT_FAILURE);
-	}
 
 	if(send(socket_pair[1], nlmsg_hdr(msg), nlmsg_hdr(msg)->nlmsg_len, 0) < 0) {
 		perror("send() failed");
@@ -156,8 +158,10 @@ static const uint8_t dummy2_mac[] = { 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff };
 	}
 
 	ret = get_mac2(socket_pair[0], "eth0", result_mac, true);
-	close(socket_pair[1]);
 	ck_assert_int_eq(0, ret);
-	ck_assert_mem_eq(dummy2_mac, result_mac, sizeof(result_mac));
+	ck_assert_mac_is_addr(result_mac, dummy2_addr);
 
 	nlmsg_free(msg);
+
+#main-pre
+	tcase_add_checked_fixture(tc1_1, netlink_setup, netlink_teardown);
